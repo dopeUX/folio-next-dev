ace3fa5214d767163be26c33e6fbb51aa90840ac
UXNation <uxnation.inc@gmail.com>
2024-01-31 15:29:06 +0530

    final

--- src/app/Common/SmoothScrollProvider/SmoothScrollProvider.tsx
+++
@@ -1,8 +0,0 @@
-import ReactLenis from "@studio-freight/react-lenis";
-import React from "react";
-
-const SmoothScrollProvider: React.FC<any> = ({ children }) => {
-  return <ReactLenis root>{children}</ReactLenis>;
-};
-
-export default SmoothScrollProvider;
